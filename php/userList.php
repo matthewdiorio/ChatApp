aad8c5f8001bd955075a94305b3cd7a5b3a56587
Matthew Diorio <matthewdiorio@icloud.com>
2021-03-15 23:38:03 -0400

    Removed extra code

--- php/userList.php
+++ php/userList.php
@@ -14,22 +14,4 @@ while ($row = mysqli_fetch_assoc($sql)) {
                         </div> ';
 }
 
-/*
-<a href="chat.php?user_id='.$row['unique_id'] .'" alt="">
-<div class="content">
-<img src="php/images/'. $row['profilePhoto'] .'" alt="">
-<div class="details">
-<span>'. $row['firstName'] . ' ' . $row['lastName'] . '</span>
-
-</div>
-</div>
-<div class="status-dot ' . $offline . '"><i class="fas fa-circle"></i></div>
-</a>
-
-
-
-
-                            <div class="thumbnail" style="background-image: url(php/images/'.$row['profilePhoto'].');>
-
- */
 ?>
